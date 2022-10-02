57720464c0cf1a8c459ba94849569e2b6ab91551
bokigolic <bokigolic32@gmail.com>
2022-10-02 01:15:55 -0400

    crate Navbar, Footer and Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
+import React, { useState } from 'react';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { Routes } from './components/Routes';
 
 
 
-function App() {
+const App = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
   return (
-    <div className="App">
-     App
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className="bg-gray-200 dark:bg-gray-900">
+      app
+      </div>
     </div>
+
   );
-}
+};
 
 export default App;
